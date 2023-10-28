f9a05a1eddfc8a0f14fa86d0844576ee0e5f3cd5
Afonso Bernardes <84087794+AfonsoBernardes@users.noreply.github.com>
2023-10-28 10:48:40 +0000

    Created 'profile.js' view and added route to 'layout.js'

--- src/front/js/layout.js
+++ src/front/js/layout.js
@@ -10,6 +10,7 @@ import injectContext from "./store/appContext";
 
 import { Navbar } from "./component/navbar";
 import { Footer } from "./component/footer";
+import { Profile } from "./pages/profile";
 
 //create your first component
 const Layout = () => {
@@ -26,6 +27,7 @@ const Layout = () => {
                     <Navbar />
                     <Routes>
                         <Route element={<Home />} path="/" />
+                        <Route element={<Profile />} path="/profile" />
                         <Route element={<Demo />} path="/demo" />
                         <Route element={<Single />} path="/single/:theid" />
                         <Route element={<h1>Not found!</h1>} />
---
+++ src/front/js/pages/profile.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Profile = () => {
+    return (  
+        <div className="profile container-fluid">
+            sdfgdg
+        </div>
+    );
+}
