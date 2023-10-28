8bc22d65815cc514807ad23b20f029c209e74629
Afonso Bernardes <84087794+AfonsoBernardes@users.noreply.github.com>
2023-10-28 11:58:13 +0000

    Added 'Profile Page' structure and default image.

--- src/front/js/pages/profile.js
+++ src/front/js/pages/profile.js
@@ -1,9 +1,19 @@
 import React from "react";
 
 export const Profile = () => {
-    return (  
-        <div className="profile container-fluid">
-            sdfgdg
+  return (
+    <div className="profile container-fluid border border-danger">
+      <div className="user-info row d-flex justify-content-between border border-danger p-1">
+
+        <div className="col-sm-12 col-md-4 border border-success">
+            <img className="profile-picture img-fluid img-thumbnail rounded-circle" src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.clipartkey.com%2Fmpngs%2Fm%2F146-1461473_default-profile-picture-transparent.png&f=1&nofb=1&ipt=f24cf99dc485c8021cf728a7612d68fe2d2b9805182ffd1cec94563521e9169b&ipo=images" alt="Profile Picture" />
         </div>
-    );
-}
+
+        <div className="col-sm-12 col-md-8 border border-primary">
+            ewrewrrr
+        </div>
+
+      </div>
+    </div>
+  );
+};
