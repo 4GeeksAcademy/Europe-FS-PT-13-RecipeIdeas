967fec76528da3bb12f2a33c49b3142f6e98d501
Cláudia Diogo <138239815+clsofia@users.noreply.github.com>
2023-10-30 21:37:17 +0000

    Added two links in the navbar, one for Creat an Account, other to login. Building the sign up page.

---
+++ src/front/js/pages/signup.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "../../styles/signup.css";
+
+export const Signup = () => {
+  return (
+    <div className="image">
+      <img className="img" alt="Image" src="https://c.animaapp.com/C971MgGw/img/image-8.png" />
+    </div>
+  );
+};
+
