93321199440d0284f9b5e231100e950033f805cf
Afonso Bernardes <84087794+AfonsoBernardes@users.noreply.github.com>
2023-11-01 19:27:05 +0000

    Making user info available in entire website thorugh Context

--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -2,24 +2,17 @@ const getState = ({ getStore, getActions, setStore }) => {
 	return {
 		store: {
 			message: null,
-			demo: [
-				{
-					title: "FIRST",
-					background: "white",
-					initial: "white"
-				},
-				{
-					title: "SECOND",
-					background: "white",
-					initial: "white"
-				}
-			]
+			
+			firstName: "Afonso",
+    		lastName: "Bernardes",
+    		username: "afonso_bernardes",
+
+    		email: "afonso.duarte.bernardes@gmail.com",
+    		linkedIn: "https://www.linkedin.com/in/afonso-bernardes/",
+    		github: "https://github.com/AfonsoBernardes",
 		},
+
 		actions: {
-			// Use getActions to call a function within a fuction
-			exampleFunction: () => {
-				getActions().changeColor(0, "green");
-			},
 
 			getMessage: async () => {
 				try{
@@ -33,20 +26,6 @@ const getState = ({ getStore, getActions, setStore }) => {
 					console.log("Error loading message from backend", error)
 				}
 			},
-			changeColor: (index, color) => {
-				//get the store
-				const store = getStore();
-
-				//we have to loop the entire demo array to look for the respective index
-				//and change its color
-				const demo = store.demo.map((elm, i) => {
-					if (i === index) elm.background = color;
-					return elm;
-				});
-
-				//reset the global store
-				setStore({ demo: demo });
-			}
 		}
 	};
 };
