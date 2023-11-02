38d96aacdc7e1b9a1e8465317515ee146f079771
Broasdemilho <135333779+Broasdemilho@users.noreply.github.com>
2023-11-02 20:10:15 +0000

    added icon to the card and worked on their position

--- src/front/js/component/recipeCard.js
+++ src/front/js/component/recipeCard.js
@@ -7,7 +7,7 @@ export const RecipeCard = () => {
 	const [isFavorite, setIsFavorite] = useState(false);
 	const [recipe, setRecipe] = useState();
 	const [euros, setEuros] = useState("");
-	const [diets, setDiets] = useState("");
+	const [diets, setDiets] = useState("Omnivore");
 
 
 
@@ -38,6 +38,7 @@ export const RecipeCard = () => {
 				console.log(data);
 				setRecipe(data);
 				totalRecipePrice(data);
+				dietDisplay(data);
 			})
 			.catch((error) => {
 				console.error('There was a problem with the fetch operation:', error);
@@ -61,19 +62,29 @@ export const RecipeCard = () => {
 		}
 		else if (price <= 10) {
 			setEuros("€€")
-	
+
 		}
 		else if (price <= 15) {
 			setEuros("€€€")
 		}
-		else { setEuros("€€€€")
-	}
+		else {
+			setEuros("€€€€")
+		}
 	}
 
+	const dietDisplay = (recipeData) => {
+		if (recipeData?.recipes[0]?.diets.length === 0) {
+			setDiets("Omnivore");
+		} else {
+			setDiets(recipeData?.recipes[0]?.diets.join(", "));
+		}
+	};
+
+{/* <div className="col-sm-12 col-md-12 col-lg-4 mb-2"></div> */}
 
 
 	return (
-		<div className="border mt-4 rounded-top" style={{ width: "18rem" }}>
+		<div className="border mt-4 rounded-top">
 			<div className="row">
 				<div className="col-9">
 					<h5 className="m-2">{recipe?.recipes[0]?.title}</h5>
@@ -83,12 +94,23 @@ export const RecipeCard = () => {
 				</div>
 			</div>
 			<img src={recipe?.recipes[0]?.image} className="card-img-top" alt="Recipe Image" />
-			<div className="">
-				<p className="mt-1 ms-2">Serving: {recipe?.recipes[0]?.servings}</p>
-				<p className="mt-1 ms-2">Total Cook Time: {recipe?.recipes[0]?.readyInMinutes} minutes</p>
-				<p className="mt-1 ms-2">Price: {euros}</p>
-				<p className="mt-1 ms-2">Diet: {Array.isArray(recipe?.recipes[0]?.diets) ? recipe?.recipes[0]?.diets.join(", ")  : "Omnivore"}</p>
-				<button type="button" class="btn btn-primary w-100">Go to Recipe</button>
+			<div className="row">
+				<div className="col-4">
+					<p className="mt-1 ms-2"><i class="fas fa-utensils fa-lg"></i> {recipe?.recipes[0]?.servings} servings</p>
+				</div>
+				<div className="col-4">
+					<p className="mt-1 ms-2"><i class="far fa-clock fa-lg"></i> {recipe?.recipes[0]?.readyInMinutes} minutes</p>
+				</div>
+				<div className="col-4">
+					<p className="mt-1 ms-2"><i class="fas fa-coins fa-lg"></i> {euros}</p>
+				</div>
+				<div />
+				<div className="d-flex justify-content-center">
+					<p className="mt-1 ms-2"><i class="fas fa-apple-alt fa-lg"></i> {diets}</p>
+				</div>
+				<div className="d-flex justify-content-center">
+					<button type="button" class="btn btn-primary w-100">Go to Recipe</button>
+				</div>
 			</div>
 		</div >
 	);
