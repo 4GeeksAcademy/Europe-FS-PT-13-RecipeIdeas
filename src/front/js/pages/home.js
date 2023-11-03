9c386b605f5282b4e686fa205f603a25e8e83c10
Broasdemilho <135333779+Broasdemilho@users.noreply.github.com>
2023-11-03 20:02:47 +0000

    added basic implemntation of the random recipe feature

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -2,25 +2,38 @@ import React, { useContext } from "react";
 import { Context } from "../store/appContext";
 import rigoImageUrl from "../../img/rigo-baby.jpg";
 import "../../styles/home.css";
+import { RecipeCard } from "../component/recipeCard";
 
 export const Home = () => {
 	const { store, actions } = useContext(Context);
 
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!!</h1>
-			<p>
-				<img src={rigoImageUrl} />
-			</p>
-			<div className="alert alert-info">
-				{store.message || "Loading message from the backend (make sure your python backend is running)..."}
+		<>
+			<div className="text-center mt-5">
+				<h1>Hello Rigo!!</h1>
+				<p>
+					<img src={rigoImageUrl} />
+				</p>
+				<div className="alert alert-info">
+					{store.message || "Loading message from the backend (make sure your python backend is running)..."}
+				</div>
+				<p>
+					This boilerplate comes with lots of documentation:{" "}
+					<a href="https://start.4geeksacademy.com/starters/react-flask">
+						Read documentation
+					</a>
+				</p>
 			</div>
-			<p>
-				This boilerplate comes with lots of documentation:{" "}
-				<a href="https://start.4geeksacademy.com/starters/react-flask">
-					Read documentation
-				</a>
-			</p>
-		</div>
-	);
+			<div className="container">
+				<div className="d-flex justify-contents-center">
+					<h1 className="">Random Recipes:</h1>
+				</div>
+				<div className="row">
+					<ul className="container" style={{listStyleType: "none"}}>
+						<RecipeCard Url="https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/random" index="0" />
+					</ul>
+				</div>
+			</div>
+		</>
+	);	
 };
