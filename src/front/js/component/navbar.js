d1683d09e32a6455dd864d613f20c7ab7dd08626
Cláudia Diogo <138239815+clsofia@users.noreply.github.com>
2023-11-12 18:13:07 +0000

    finishing log out

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -7,26 +7,30 @@ import food from "../../img/food.png";
 
 
 export const Navbar = () => {
-	const { store, actions} = useContext(Context);
+	const { store, actions } = useContext(Context);
 	return (
-	  <nav className="navbar">
-		<div className="text-wrapper"><img src={logo} style={{width:"50px", height:"43px"}}></img><img src = {food}style={{width:"200px", height:"43px"}} />
-		</div>
-		
-	
-		<div className="sign-up-button">
-		<Link className= 'nav-link' to="/signup">Sign Up</Link>
-		</div>
-		
-		<div className="log-in-button">
-			{!store.token?
+		<nav className="navbar">
+			<div className="text-wrapper"><img src={logo} style={{ width: "50px", height: "43px" }}></img><img src={food} style={{ width: "200px", height: "43px" }} />
+			</div>
 
-		<Link className= 'nav-link' to="/login">Log In</Link>
-		:
-		<button onClick={() => actions.logout()} className="btn">Log out</button>
-	}
-		</div>
+			{!store.token ?
+				<>
+					<div className="sign-up-button">
+						<Link className='nav-link' to="/signup">Sign Up</Link>
+					</div>
 
-	  </nav>
+					<div className="log-in-button">
+
+						<Link className='nav-link' to="/login">Log In</Link>
+
+					</div>
+				</>
+				:
+				<div className="log-in-button">
+					<button onClick={() => actions.logout()} className="btn">Log out</button>
+				</div>
+			}
+
+		</nav>
 	);
-  };
+};
--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -1,6 +1,7 @@
 const getState = ({ getStore, getActions, setStore }) => {
 	return {
 		store: {
+			token: null,
 			message: null,
 			demo: [
 				{
@@ -20,77 +21,87 @@ const getState = ({ getStore, getActions, setStore }) => {
 			exampleFunction: () => {
 				getActions().changeColor(0, "green");
 			},
-			
 
-			getToken: () =>{
-				return sessionStorage.getItem("token");
-				
+
+			getToken: () => {
+				if (!getStore().token) {
+					const token = sessionStorage.getItem("token");
+					if (token) {
+						setStore({ token: token });
+					}
+					return token
+				}
+
+
 			},
 
-			logout: () =>{
+			logout: () => {
 				sessionStorage.removeItem("token");
 				console.log("Log out");
-				setStore({token : null});
+				setStore({ token: null });
 
 			},
 
 			login: async (email, password) => {
-				const opts ={
+				const opts = {
 					method: "POST",
-					headers:{ "Content-Type": "application/json"
-				  },
-					body:JSON.stringify(
-					{
-					  "email": email,
-					  "password": password
-					 })
-				  }
-			  
-				 return fetch(process.env.BACKEND_URL + "/api/token", opts)
-			
-				.then(resp =>{
-				  if(resp.status === 200) return resp.json();
-				  else return false
-				})
-				.then(data => {
-				  console.log("this came from the backend", data)
-					sessionStorage.setItem("token", data.access_token);
-					return true
-				})
-				.catch(error =>{
-				  console.log("There was an error", error)
-				  return false
-				})
+					headers: {
+						"Content-Type": "application/json"
+					},
+					body: JSON.stringify(
+						{
+							"email": email,
+							"password": password
+						})
+				}
+
+				return fetch(process.env.BACKEND_URL + "/api/token", opts)
+
+					.then(resp => {
+						if (resp.status === 200) return resp.json();
+						else return false
+					})
+					.then(data => {
+						console.log("this came from the backend", data)
+						sessionStorage.setItem("token", data.access_token);
+						setStore({ token: data.access_token })
+						return true
+					})
+					.catch(error => {
+						console.log("There was an error", error)
+						return false
+					})
 			},
 
 			signup: (name, email, password) => {
-				const opts ={
+				const opts = {
 					method: "POST",
-					headers:{ "Content-Type": "application/json"
-				  },
-					body:JSON.stringify(
-					{
-					  "name" : name,
-					  "email": email,
-					  "password": password
-					 })
-				  }
-			  
-				return fetch(process.env.BACKEND_URL + "/api/signup", opts)
-				.then(resp =>{
-				  if(resp.status === 200) return resp.json();
-				  else return false;
+					headers: {
+						"Content-Type": "application/json"
+					},
+					body: JSON.stringify(
+						{
+							"name": name,
+							"email": email,
+							"password": password
+						})
+				}
 
-				})
-				.then(data => {
-				  console.log("sign up successful", data)
-				  return true;
-					
-				})
-				.catch(error =>{
-				  console.log("There was an error", error)
-				  return false;
-				})
+				return fetch(process.env.BACKEND_URL + "/api/signup", opts)
+					.then(resp => {
+						if (resp.status === 200) return resp.json();
+						else return false;
+
+					})
+					.then(data => {
+						console.log("sign up successful", data)
+						return true;
+
+					})
+					.catch(error => {
+						console.log("There was an error", error)
+						return false;
+					})
 			},
 			getMessage: () => {
 				const store = getStore();
@@ -99,13 +110,13 @@ const getState = ({ getStore, getActions, setStore }) => {
 						"Authorization": "Bearer" + store.token
 					}
 				};
-					// fetching data from the backend
-					fetch(process.env.BACKEND_URL + "/api/hello", opts)
+				// fetching data from the backend
+				fetch(process.env.BACKEND_URL + "/api/hello", opts)
 					.then(resp => resp.json())
 					.then(data => setStore({ message: data.message }))
 					// don't forget to return something, that is how the async resolves
 					.catch(error => console.log("Error loading message from backend", error));
-				
+
 			},
 
 
