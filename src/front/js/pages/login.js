5f90dcb2ae7b61899040d273ac5b3860f9909bfe
Cláudia Diogo <138239815+clsofia@users.noreply.github.com>
2023-11-13 12:43:18 +0000

    login adjust and migration

--- src/front/js/pages/login.js
+++ src/front/js/pages/login.js
@@ -5,7 +5,7 @@ import { useNavigate } from "react-router-dom";
 
 
 export const Login = () => {
-    const {store, actions} = useContext(Context);
+  const { store, actions } = useContext(Context);
 
   const [email, setEmail] = useState("");
 
@@ -13,49 +13,49 @@ export const Login = () => {
 
   const navigate = useNavigate();
 
-  
+
   const handleSubmit = (e) => {
     e.preventDefault();
     const loginResult = actions.login(email, password)
-    loginResult.then( result => {
-      if (result === true){
-        navigate("/home")
+    loginResult.then(result => {
+      if (result === true) {
+        navigate("/")
       }
-      else alert ("Have you register already?")
+      else alert("Have you register already?")
     })
   };
-  
-  return (
-    
-    <div>
-    <h2>Login</h2>
 
-    {(actions.getToken() && actions.getToken()!="" && actions.getToken()!=undefined) ? "You are logged in with this token" + actions.getToken() :
-    
-    <form onSubmit={handleSubmit}>
-      <div>
-        <label>Email:</label>
-        <input
-          type="text"
-          placeholder="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-      </div>
-      <div>
-        <label>Password:</label>
-        <input
-          type="password"
-          placeholder="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-      </div>
-      <div>
-      <input type="submit" value="Login" />
-      </div>
-    </form>
-    }
-  </div>
+  return (
+
+    <div>
+      <h2>Login</h2>
+
+      {(actions.getToken() && actions.getToken() != "" && actions.getToken() != undefined) ? "You are logged in with this token" + actions.getToken() :
+
+        <form onSubmit={handleSubmit}>
+          <div>
+            <label>Email:</label>
+            <input
+              type="text"
+              placeholder="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+          <div>
+            <label>Password:</label>
+            <input
+              type="password"
+              placeholder="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+          <div>
+            <input type="submit" value="Login" />
+          </div>
+        </form>
+      }
+    </div>
   );
 };
